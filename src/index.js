1ddb82a1315ff81552581221dfea72ac3f610fec
chalam <dim2dip@gmail.com>
2017-05-28 15:22:58 -0400

    add nomatch and redirects

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch,
+  Redirect,
 } from 'react-router-dom'
 import JumbotronGrid from './JumbotronGrid';
 import Footer from './Footer';
@@ -29,30 +31,41 @@ const NoBody = () => (
   </div>
 )
 
+const NoMatch = ({ location }) => (
+  <div>
+    <h3>No match for <code>{location.pathname}</code></h3>
+  </div>
+)
+
+
 // Each logical "route" has two components, one for
 // the sidebar and one for the main area. We want to
 // render both of them in different places when the
 // path matches the current URL.
 const routes = [
   {
-    path: '/',
+    path: '/app',
     exact: true,
     sidebar: () => <div>home!</div>,
     main: () => <App/>
   }, {
     path: '/jumbo',
+    exact: true,
     sidebar: () => <div>jumbotronBody!</div>,
     main: () => <JumbotronBody/>
   }, {
     path: '/noBody',
+    exact: true,
     sidebar: () => <div>noBody!</div>,
     main: () => <NoBody/>
   }, {
     path: '/repos',
+    exact: true,
     sidebar: () => <div>noBody!</div>,
     main: () => <Repos/>
   }, {
     path: '/about',
+    exact: true,
     sidebar: () => <div>noBody!</div>,
     main: () => <About/>
   }
@@ -61,6 +74,7 @@ const routes = [
 ReactDOM.render(
   <Router>
     <div>
+      <Switch>
       {routes.map((route, index) => (
         // Render more <Route>s with the same paths as
         // above, but different components this time.
@@ -71,6 +85,10 @@ ReactDOM.render(
           component={route.main}
         />
       ))}
+
+      <Redirect from="/" to="/app" />
+      <Route component={NoMatch} />
+    </Switch>
     </div>
   </Router>, document.getElementById('root'));
 registerServiceWorker();
